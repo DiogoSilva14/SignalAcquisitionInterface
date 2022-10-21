c7f0466c598e32c89e54b35b88447f6ba87bf23b
Diogo Silva <diogo.silva.pt14@gmail.com>
2022-10-21 22:39:31 +0100

    Created Modbus library with initialize, send frame and crc functions

--- Gateway/Modbus_Implementation/modbus.c
+++ Gateway/Modbus_Implementation/modbus.c
@@ -1,1 +1,60 @@
 #include "modbus.h"
+
+uint8_t device_address = 0;
+uint8_t initialized = 0;
+int baud_rate = 0;
+
+int init_modbus(int baud_rate, uint8_t address){
+    if(init_rs485(baud_rate)){
+        return FAILED_TO_INITIALIZE;
+    }
+
+    device_address = address;
+    baud_rate = baud_rate;
+
+    initialized = 1;
+
+    return 0;
+}
+
+int send_frame(uint8_t destination_address, uint8_t function, uint8_t* data_pointer, uint8_t data_length){
+    if(!initialized){
+        return NOT_INITIALIZED;
+    }
+
+    uint8_t frame_buffer[data_length + 3];
+    uint8_t frame_it = 0;
+
+    frame_buffer[frame_it++] = destination_address;
+    frame_buffer[frame_it++] = function;
+
+    for(int i=0; i < data_length; i++){
+        frame_buffer[frame_it++] = data_pointer[i];
+    }
+
+    uint16_t crc = crc16(frame_buffer, frame_it);
+
+    frame_buffer[frame_it++] = crc & 0xFF;
+    frame_buffer[frame_it++] = crc >> 16;
+
+    delay_us(ceil(28000/baud_rate));
+
+    for(int i=0; i < frame_it; i++){
+        sendByte(frame_buffer[i]);
+    }
+
+    delay_us(ceil(28000/baud_rate));
+}
+
+uint16_t crc16(const uint16_t* data_pointer, int length){
+    unsigned char x;
+    unsigned short crc = 0xFFFF;
+
+    while (length--){
+        x = crc >> 8 ^ *data_p++;
+        x ^= x>>4;
+        crc = (crc << 8) ^ ((unsigned short)(x << 12)) ^ ((unsigned short)(x <<5)) ^ ((unsigned short)x);
+    }
+
+    return crc;
+}
--- Gateway/Modbus_Implementation/modbus.h
+++ Gateway/Modbus_Implementation/modbus.h
@@ -1,15 +1,52 @@
-#ifndef MODBUS_H
-#define MODBUS_H
+/** @file modbus.h
+ *  @brief Modbus implementation
+ *
+ *  This library is a standard implementation of
+ *  Modbus RTU
+ *
+ *  @author Diogo Silva
+ *  @bug No known bugs.
+ */
+
+#ifndef _MODBUS_H
+#define _MODBUS_H
+
+#include <stdio.h>
+#include "rs485.h"
+#include "device_functions.h"
+
+#define FAILED_TO_INITIALIZE -1
+#define NOT_INITIALIZED -1
+#define ERROR_SENDING_FRAME -2
+
+/** @brief Initialize modbus module
+ *
+ *  @param baud_rate Baud rate desired for RS-485 module
+ *  @param address Address intended for this device on Modbus line
+ *  @return 0 Modbus module was initialized properly
+ *          -1 Failed to initialize Modbus module
+ */
+int init_modbus(int baud_rate, uint8_t address);
 
 
+/** @brief Send a frame over Modbus
+ *
+ *  @param destination_address Destination device's address
+ *  @param function Function intended for the frame
+ *  @param data_pointer Pointer to the data buffer
+ *  @param data_length Data Buffer length
+ *  @return 0 Frame sent
+ *          -1 Modbus module not initialized
+ *          -2 Modbus frame not sent
+ */
+int send_frame(uint8_t destination_address, uint8_t function, uint8_t* data_pointer, uint8_t data_length);
 
-
-
-
-
-
-
-
-
+/** @brief Calculates the 16-bit CRC
+ *
+ *  @param data_pointer Pointer to the data buffer to calculate the crc
+ *  @param data_length Length of data buffer
+ *  @return Calculated CRC
+ */
+uint16_t crc16(const uint16_t* data_pointer, int length);
 
 #endif
